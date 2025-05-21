9c774ff4ba09ed28d19c50eca67ad3b75e6324fa
Stefan Dimitrijevic <cph-sd77@cphbusiness.dk>
2025-05-21 22:55:27 +0200

    feat: basic navbar component with routing logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import FacilityList from "../pages/FacilityList/FacilityList";
 import FacilityForm from "../pages/FacilityForm/FacilityForm";
+import MainLayout from "../layout/MainLayout";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<FacilityList />} />
-      <Route path="/create" element={<FacilityForm />} />
-      <Route path="/edit/:id" element={<FacilityForm />} />
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<FacilityList />} />
+        <Route path="/create" element={<FacilityForm />} />
+        <Route path="/edit/:id" element={<FacilityForm />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
     </Routes>
   );
 };
